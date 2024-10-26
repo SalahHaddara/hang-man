b204c8f7f25d45ed8f879aab38068b52303d3b1c
Salah Haddara <Salahhaddara02@gmail.com>
2024-10-26 07:41:24 +0300

    wordDisplay completed

--- scripts/game.js
+++ scripts/game.js
@@ -4,10 +4,48 @@ function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
 }
 
+lives = 6;
 var randomWord = getRandomWord();
 var answerSection = document.getElementById("answer-section");
 var dashes = "-".repeat(randomWord.length);
 
 answerSection.textContent = dashes;
 
+var lettersSelected = document.querySelectorAll('.letter');
 
+lettersSelected.forEach((letterElement) => {
+    lettersSelected.addEventListener('click', event => {
+        var letter = event.target.textContent;
+
+        lettersSelected.style.opacity = '0.5';
+        lettersSelected.style.pointerEvents = 'none';
+        clickHandler(letter, letterElement);
+    })
+})
+
+function clickHandler(letter, letterElement) {
+    if (randomWord.includes(letter)) {
+        updateWordDisplay(letter);
+    } else {
+        lives--;
+        //update hangman
+        if (lives === 0) {
+            alert("Game Over! The word was " + randomWord);
+        }
+    }
+}
+
+function updateWordDisplay(letter) {
+    var answerSection = document.getElementById("answer-section");
+    var displayedWord = answerSection.textContent.split('');
+    for (let i = 0; i < randomWord.length; i++) {
+        if (randomWord[i] === letter) {
+            displayedWord[i] = letter;
+        }
+    }
+    answerSection.textContent = displayedWord.join('');
+
+    if (displayedWord.includes('-') === false) {
+        alert("You Guessed it!" + randomWord);
+    }
+}
